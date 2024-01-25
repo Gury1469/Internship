5dfdd4f57416e54cb1778bd5a8da71b1d0cb22e3
Gurkirtan Singh <gurkirtan1469@gmail.com>
2024-01-25 13:01:06 +0530

    started new project p2

---
+++ pm/src/Components/ToDoItem2.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function ToDoItem2() {
+  return (
+    <>
+        
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>
+              <tr className="row">
+                <td className="col-9">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing
+                </td>
+                <td className="col-3">
+                  <button type="button" class="btn btn-outline-danger">
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            </td>
+          </tr>
+    </>
+  )
+}
--- pm/src/Components/ToDoList.jsx
+++ pm/src/Components/ToDoList.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ToDoListHead from "./ToDoListHead";
+import ToDoItem1 from "./ToDoItem1";
 export default function ToDoList() {
   return (
     <div className="pt-4">
@@ -12,38 +12,7 @@ export default function ToDoList() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td className="col-2">1</td>
-            <td className="col-2">Jacob</td>
-            <td className="col-8">
-              <tr className="row">
-                <td className="col-9">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing
-                </td>
-                <td className="col-3">
-                  <button type="button" class="btn btn-outline-danger">
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            </td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Jacob</td>
-            <td>
-              <tr className="row">
-                <td className="col-9">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing
-                </td>
-                <td className="col-3">
-                  <button type="button" class="btn btn-outline-danger">
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            </td>
-          </tr>
+          <ToDoItem1/>
         </tbody>
       </table>
     </div>
